858f8b55434cbbe80b8bfa5f989a29022323088f
pdn002 <pdn002@ucsd.edu>
2024-03-15 12:01:03 -0700

    added Devanshi's code to notebook

--- materials_and_contrifactors.py
+++ materials_and_contrifactors.py
@@ -7,86 +7,87 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('Spill_Incidents.csv')
+def material_and_contrifactors(filepath):
+    data = pd.read_csv(filepath)
 
 
-# In[2]:
+    # In[2]:
 
 
-# Remove empty cells from the cols 
-data = data.dropna(subset=['Contributing Factor', 'Material Name','Material Family'])
+    # Remove empty cells from the cols 
+    data = data.dropna(subset=['Contributing Factor', 'Material Name','Material Family'])
 
 
-# In[3]:
+    # In[3]:
 
 
-unique_values_set = set()
-unique_values_set.update(data['Contributing Factor'].unique())
+    unique_values_set = set()
+    unique_values_set.update(data['Contributing Factor'].unique())
 
-print(unique_values_set)
-
-
-# In[4]:
+    print(unique_values_set)
 
 
-# Combine Unknown,Other and Missing Code in Old Data - Must be fixed to Unknown 
-data['Contributing Factor'] = data['Contributing Factor'].replace(['Other','Missing Code in Old Data - Must be fixed'], 'Unknown')
+    # In[4]:
 
 
-# In[7]:
+    # Combine Unknown,Other and Missing Code in Old Data - Must be fixed to Unknown 
+    data['Contributing Factor'] = data['Contributing Factor'].replace(['Other','Missing Code in Old Data - Must be fixed'], 'Unknown')
 
 
-data['Material Name'] = data['Material Name'].str.replace('.*fuel oil.*', 'fuel oil', regex=True)
-data['Material Name'] = data['Material Name'].str.replace('.*other.*', 'other', regex=True)
+    # In[7]:
 
 
-# In[18]:
+    data['Material Name'] = data['Material Name'].str.replace('.*fuel oil.*', 'fuel oil', regex=True)
+    data['Material Name'] = data['Material Name'].str.replace('.*other.*', 'other', regex=True)
 
 
-# Plot of Contributing Factors
-cf_count = data['Contributing Factor'].value_counts()
-
-# Plot the graph
-plt.figure(figsize=(10, 6))
-cf_count.plot(kind='bar', color='skyblue')
-plt.title('Leading contributors of the spills')
-plt.xlabel('Contributing Factors')
-plt.ylabel('Number of spills')
-plt.show()
+    # In[18]:
 
 
-# In[25]:
+    # Plot of Contributing Factors
+    cf_count = data['Contributing Factor'].value_counts()
+
+    # Plot the graph
+    plt.figure(figsize=(10, 6))
+    cf_count.plot(kind='bar', color='skyblue')
+    plt.title('Leading contributors of the spills')
+    plt.xlabel('Contributing Factors')
+    plt.ylabel('Number of spills')
+    plt.show()
 
 
-mn_high = data['Material Name'].value_counts()
-
-# Filter values with count > 100
-filtered_values = mn_high.head()
-
-# Plot the graph
-plt.figure(figsize=(10, 6))
-filtered_values.plot(kind='bar', color='skyblue')
-plt.title('Top 5 materials spilt')
-plt.xlabel('Material Name')
-plt.ylabel('Frequency')
-plt.show()
+    # In[25]:
 
 
-# In[16]:
+    mn_high = data['Material Name'].value_counts()
+
+    # Filter values with count > 100
+    filtered_values = mn_high.head()
+
+    # Plot the graph
+    plt.figure(figsize=(10, 6))
+    filtered_values.plot(kind='bar', color='skyblue')
+    plt.title('Top 5 materials spilt')
+    plt.xlabel('Material Name')
+    plt.ylabel('Frequency')
+    plt.show()
 
 
-# Plot of Material Name
-mf_count = data['Material Family'].value_counts()
-
-# Plot the graph
-plt.figure(figsize=(10, 6))
-mf_count.plot(kind='pie', autopct='%1.1f%%', colors=['skyblue', 'lightcoral', 'lightgreen', 'orange', 'lightpink'])
-plt.title('Frequency of each material family in the spills')
-plt.ylabel('')
-plt.show()
+    # In[16]:
 
 
-# In[ ]:
+    # Plot of Material Name
+    mf_count = data['Material Family'].value_counts()
+
+    # Plot the graph
+    plt.figure(figsize=(10, 6))
+    mf_count.plot(kind='pie', autopct='%1.1f%%', colors=['skyblue', 'lightcoral', 'lightgreen', 'orange', 'lightpink'])
+    plt.title('Frequency of each material family in the spills')
+    plt.ylabel('')
+    plt.show()
+
+
+    # In[ ]:
 
 
 
